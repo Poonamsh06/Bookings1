e9a9fed75e9c93bf397297c087df8e1af1832e97
unknown <poonamsh061002@gmail.com>
2024-02-12 16:24:31 +0530

    Booking form

--- lib/widgets/textformfield.dart
+++ lib/widgets/textformfield.dart
@@ -73,7 +73,225 @@ class LoginFormField extends StatelessWidget {
         ),
         contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
       ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter some text';
+        }
+        // You can add more validation logic here as per your requirement
+        return null; // Returning null means the input is valid
+      },
     );
-    ;
+  }
+}
+
+class BookingField extends StatelessWidget {
+  final String hintText;
+  final String labelText;
+  const BookingField({
+    super.key,
+    required this.hintText,
+    required this.labelText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          labelText,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.normal, fontSize: 19),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.015,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: MediaQuery.of(context).size.height * 0.08,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: TextFormField(
+            // controller: controller,
+            decoration: InputDecoration(
+              focusColor: Color.fromARGB(255, 5, 49, 84),
+              //  hintText: hintText,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Color.fromARGB(255, 5, 49, 84)),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 5, 49, 84),
+                  width: 2.0,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                  width: 1.5,
+                ),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text';
+              }
+              // You can add more validation logic here as per your requirement
+              return null; // Returning null means the input is valid
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BookingField1 extends StatelessWidget {
+  DateTime? selectedDate = DateTime.now();
+  final String labelText;
+  final String text;
+  BookingField1({super.key, required this.labelText, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.normal, fontSize: 19),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.015,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: MediaQuery.of(context).size.height * 0.08,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: TextFormField(
+            // controller: controller,
+            // readOnly: true,
+            onTap: () async {
+              final DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: selectedDate!,
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2101),
+              );
+              if (pickedDate != null && pickedDate != selectedDate) {
+                selectedDate = pickedDate;
+              }
+            },
+
+            decoration: InputDecoration(
+              suffixIcon: Icon(Icons.calendar_month),
+              focusColor: Color.fromARGB(255, 5, 49, 84),
+              //  hintText: hintText,
+              hintText: labelText,
+              hintStyle: TextStyle(color: Color.fromARGB(255, 5, 49, 84)),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 5, 49, 84),
+                  width: 2.0,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                  width: 1.5,
+                ),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text';
+              }
+              // You can add more validation logic here as per your requirement
+              return null; // Returning null means the input is valid
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BookingField2 extends StatelessWidget {
+  final String text;
+  //DateTime? selectedDate = DateTime.now();
+  final String labelText;
+  BookingField2({super.key, required this.labelText, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.normal, fontSize: 19),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.015,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: MediaQuery.of(context).size.height * 0.08,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: TextFormField(
+            decoration: InputDecoration(
+              suffixIcon: Icon(Icons.schedule),
+              focusColor: Color.fromARGB(255, 5, 49, 84),
+              //  hintText: hintText,
+              hintText: labelText,
+              hintStyle: TextStyle(color: Color.fromARGB(255, 5, 49, 84)),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 5, 49, 84),
+                  width: 2.0,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Colors.grey.shade400,
+                  width: 1.5,
+                ),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text';
+              }
+              // You can add more validation logic here as per your requirement
+              return null; // Returning null means the input is valid
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
